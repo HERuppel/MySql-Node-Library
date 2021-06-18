31b8fe79c6c2cc082ee46d261837764d1afde579
Renato Ruppel <heruppel2@gmail.com>
2021-06-18 14:10:35 -0300

    Initial Commit. ITALOPTPMU

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false,
+  printWidth: 110,
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  endOfLine: 'crlf'
+};
